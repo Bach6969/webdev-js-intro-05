8f3a3d9b1c6b343daf876d90e825e0ccb8e7976c
bach <bachlynn23@gmail.com>
2024-12-29 18:13:00 -0500

    index part of assignment

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let groceryList = ["Milk", "Eggs", "Bread", "Butter", "Cheese"];
+console.log("Grocery List:", groceryList);
+groceryList.shift();
+console.log(groceryList);
+groceryList.push("coffee");
+console.log(groceryList);
+groceryList[1] = "Cheese";
+console.log("After changing an element:", groceryList);
+
+let mixedArray = [42, "hello", true, { key: "value" }, [1, 2, 3]];
+console.log("Mixed Array:", mixedArray);
+
+console.log("Type of mixedArray:", typeof mixedArray);
+*/
